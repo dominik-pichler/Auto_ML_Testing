1aa1cc8c31d108fbff8dd4e5b1e87cd2ba998e8b
dominik-pichler <info@dominik-pichler.com>
2024-05-17 15:57:06 +0200

    Add first stable experiment version

--- Experiment.py
+++ Experiment.py
@@ -1,8 +1,9 @@
-from mlflow import (log_metric, log_param)
+import pandas as pd
+from mlflow import (log_metric, log_param, start_run)
 import psutil
 import json
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percentage_error
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.pipeline import Pipeline
@@ -11,7 +12,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
-
+import warnings
+warnings.filterwarnings("ignore") #TODO: Remove before release
 
 class Experiment:
     def __init__(self, path_config_json):
@@ -22,23 +24,24 @@ class Experiment:
         except FileNotFoundError:
             raise FileNotFoundError("Faulty Path for Config JSON")
 
-        self.train_x = config_data["data_paths"]["train_x"]
-        self.train_y = config_data["data_paths"]["train_y"]
-        self.test_y = config_data["data_paths"]["test_x"]
-        self.test_x = config_data["data_paths"]["test_y"]
+        self.train_x = pd.read_csv(config_data["data_paths"]["train_x"])
+        self.train_y = pd.read_csv(config_data["data_paths"]["train_y"])
+        self.test_y = pd.read_csv(config_data["data_paths"]["test_y"])
+        self.test_x = pd.read_csv(config_data["data_paths"]["test_x"])
 
         self.preprocessing_steps = config_data["preprocessing_steps"]
+        self.param_grid = [config_data["param_grid"]]
 
         self.model = config_data["model"]
         self.classifier = self._get_classifier()
 
         self.HPOptimizer_type = config_data["HPOptimizer_type"]
-        self.param_grid = [config_data["param_grid"]]
         self.pipeline = config_data["pipeline"]
 
         self.tracking_uri = config_data["tracking_uri"]
         self.experiment_description = config_data["experiment_description"]
         self.experiment_name = config_data["experiment_name"]
+        self.experiment_id = config_data["experiment_id"]
 
     def _get_classifier(self):
         # Dictionary mapping model names to scikit-learn classes
@@ -53,12 +56,14 @@ class Experiment:
 
         # Get the classifier class from the dictionary
         classifier_class = model_dict.get(self.model)
+        # self.param_grid[0].update({'classifier': model_dict.get(self.model)})
 
         if classifier_class is None:
             raise ValueError(f"Model '{self.model}' is not supported.")
 
         # Initialize and return the classifier instance
         return classifier_class()
+
     def setup_preProcessor(self):
         preprocessor_transformers = []
 
@@ -70,57 +75,67 @@ class Experiment:
             remainder="drop")  # Drop any remaining columns that were not specified in the transformers
 
     def run_experiment(self):
-        # General logging for reproducability
-        log_param("_cpu_model", psutil.cpu_freq())
-        log_param("_cpu_count", psutil.cpu_count())
-        log_param("_ram_total_gb", round(psutil.virtual_memory().total / (1024.0 ** 3), 2))
+        # Setting up the MLFlow Project:
+        with start_run(experiment_id=self.experiment_id, run_name=self.experiment_name):
 
-        # Log system metrics during training
-        cpu_percent = psutil.cpu_percent()
-        ram_percent = psutil.virtual_memory().percent
+            # General logging for reproducability
+            log_param("_cpu_model", psutil.cpu_freq())
+            log_param("_cpu_count", psutil.cpu_count())
+            log_param("_ram_total_gb", round(psutil.virtual_memory().total / (1024.0 ** 3), 2))
 
-        log_metric("cpu_percent", cpu_percent)
-        log_metric("ram_percent", ram_percent)
+            # Create a Pipeline
+            pipeline = Pipeline([
+                # ("preprocessor", self.setup_preProcessor()),
+                ('classifier', self.classifier)
+            ])
 
+            search = GridSearchCV(pipeline,
+                                  param_grid=self.param_grid,
+                                  cv=3,
+                                  verbose=True
+                                  )
 
+            match self.HPOptimizer_type:
+                case 'GridSearch':
+                    search = GridSearchCV(pipeline,
+                                          param_grid=self.param_grid,
+                                          cv=3,
+                                          verbose=True
+                                          )
+                #TODO: Implement more searchers
+                case _:
+                    raise ValueError(f"Searcher '{self.model}' is not supported.")
 
+            search.fit(self.train_x, self.train_y)
 
-        # Create a Pipeline
-        pipeline = Pipeline([
-            ("preprocessor", self.setup_preProcessor()),
-            ('classifier', self.classifier)
-        ])
+            print("Best parameter (CV score=%0.3f):" % search.best_score_)
 
-        match self.HPOptimizer_type:
-            case 'GridSearchCV':
-                search = GridSearchCV(pipeline,
-                                      param_grid=self.param_grid,
-                                      cv=[(slice(None), slice(None))],
-                                      verbose=True
-                                      )
-            case _:
-                print("nothing")
-            # TODO: extend
+            # Extracting the Hyperparamter for logging from the JSON
 
-        search.fit(self.train_x, self.train_y)
+            for key in self.param_grid[0].keys():
+                log_param(f"__Hyperparamter_{key}",search.best_params_[key])
 
-        print("Best parameter (CV score=%0.3f):" % search.best_score_)
-        log_param("__Hyperparam__n_neighbors", search.best_params_["classifier__n_neighbors"])
-        log_param("__Hyperparam__weights", search.best_params_["classifier__weights"])
-        log_param("__Hyperparam__p", search.best_params_["classifier__p"])
+            #log_param("__Hyperparam__n_neighbors", search.best_params_["classifier__penalty"])
+            #log_param("__Hyperparam__weights", search.best_params_["classifier__C"])
+            #log_param("__Hyperparam__p", search.best_params_["classifier__solver"])
 
-        for param in self.param_grid:
-            log_param(str(param["name"]), search.best_params_[str(param["name"])])
+            """
+            for param in self.param_grid:
+                log_param(str(param["name"]), search.best_params_[str(param["name"])])
+            """
+            print(search.best_params_)
+            # print(search.best_params_["classifier"])
+            y_pred = search.predict(self.test_x)
 
-        print(search.best_params_)
-        print(search.best_params_["classifier"])
-        y_pred = search.predict(self.test_x)
+            # TODO: Add way to also parametrise the metrics  (CAUTION: Right now it will fail for Classification
+            log_metric("R2", r2_score(y_true=self.test_y, y_pred=y_pred))
 
-        log_metric("R^2", r2_score(y_true=self.test_y, y_pred=y_pred))
-        log_metric("MSE", mean_squared_error(y_true=self.test_y, y_pred=y_pred))
-        log_metric("MAPE", mean_absolute_percentage_error(y_true=self.test_y, y_pred=y_pred, pos_label=1), )
+            log_metric("MSE", mean_squared_error(y_true=self.test_y, y_pred=y_pred))
+            log_metric("MAPE", mean_absolute_percentage_error(y_true=self.test_y, y_pred=y_pred), )
 
 
 if __name__ == '__main__':
+    # Setup Experiment:
+
     experiment = Experiment("templates/template_config.json")
     experiment.run_experiment()
